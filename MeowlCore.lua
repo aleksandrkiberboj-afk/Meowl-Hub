7b004c9510888d3c27b1de9923e9fe505d6249c9
aleksandrkiberboj-afk <aleksandrkiberboj@gmail.com>
2026-01-09 15:29:05 +0200

    Refactor notification function and update data structure

--- MeowlCore.lua
+++ MeowlCore.lua
@@ -1,18 +1,13 @@
 local MeowlCore = {}
-
 MeowlCore.Data = {
     Creator = "Meowl_2026",
-    Info = "Meowl_2705",
-    Version = "1.0"
+    Info = "Meowl_2705"
 }
-
--- Функция уведомления через саму WindUI (мы передадим её позже)
-function MeowlCore.Notify(library, title, content)
-    library:Notify({
+function MeowlCore.Notify(lib, title, text)
+    lib:Notify({
         Title = title,
-        Content = content,
+        Content = text,
         Duration = 5
     })
 end
-
 return MeowlCore
